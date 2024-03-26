3dc5db07c1a412a5eec272f5db7f7dd1a66d8de0
Ashani9696 <ashani9996@gmail.com>
2024-03-27 00:12:16 +0530

    Initial Commit

---
+++ ClothingAppIOS/ClothingAppIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClothingAppIOSApp.swift
+//  ClothingAppIOS
+//
+//  Created by Ashani Dilanka on 2024-03-27.
+//
+
+import SwiftUI
+
+@main
+struct ClothingAppIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
