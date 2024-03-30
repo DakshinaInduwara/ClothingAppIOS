f4de69be7cf763d376120e5c09f8f178db4d5e6f
Ashani9696 <ashani9996@gmail.com>
2024-03-30 11:29:18 +0530

    modify ViewItem screen

--- ClothingAppIOS/Views/ViewItem.swift
+++ ClothingAppIOS/Views/ViewItem.swift
@@ -2,7 +2,17 @@
 //  ViewItem.swift
 //  ClothingAppIOS
 //
-//  Created by Ashani Dilanka on 2024-03-27.
+//  Created by Ashani Dilanka on 2024-03-30.
 //
 
-import Foundation
+import SwiftUI
+
+struct ViewItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewItem()
+}
