3c4b6215dab052e7f61d7bc9ee0e8666b5a5971c
Ashani9696 <ashani9996@gmail.com>
2024-03-30 08:01:06 +0530

    modify productCard

---
+++ ClothingAppIOS/Components/SelectButton.swift
@@ -0,0 +1,18 @@
+//
+//  SelectButton.swift
+//  ClothingAppIOS
+//
+//  Created by Ashani Dilanka on 2024-03-30.
+//
+
+import SwiftUI
+
+struct SelectButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectButton()
+}
